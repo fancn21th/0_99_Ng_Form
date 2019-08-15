9b0a5811dfaa09770b3ada1bf83a1235d6de0a71
alex.fan <alex.fan@okchem.com>
2019-08-15 16:27:57 +0800

    form status

--- src/app/widgets/form-three/form-three.component.ts
+++ src/app/widgets/form-three/form-three.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 
 @Component({
   selector: "app-form-three",
@@ -6,11 +6,18 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./form-three.component.css"]
 })
 export class FormThreeComponent implements OnInit {
+  @ViewChild("formRef", { static: false }) form;
+
   username = "alex fan";
 
   constructor() {}
 
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.form.valueChanges.subscribe(v => console.table(v));
+    this.form.statusChanges.subscribe(v => console.table(v));
+  }
 
   onSubmit(formValue) {
     console.log(formValue);
