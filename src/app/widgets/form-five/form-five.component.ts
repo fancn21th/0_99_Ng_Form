279d91cffd7e541a87cc3d4bdf0a4d1988f16dbf
alex.fan <alex.fan@okchem.com>
2019-08-16 11:04:06 +0800

    seletion

--- src/app/widgets/form-five/form-five.component.ts
+++ src/app/widgets/form-five/form-five.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-form-five',
-  templateUrl: './form-five.component.html',
-  styleUrls: ['./form-five.component.css']
+  selector: "app-form-five",
+  templateUrl: "./form-five.component.html",
+  styleUrls: ["./form-five.component.css"]
 })
 export class FormFiveComponent implements OnInit {
+  locations: string[] = ["Home", "Away", "Space", "Ocean"];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/widgets/form-four/form-four.component.ts
+++ src/app/widgets/form-four/form-four.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from "@angular/core";
 })
 export class FormFourComponent implements OnInit {
   locations: string[] = ["Home", "Away", "Space"];
-  selectedLocation: null;
+  selectedLocation;
 
   constructor() {}
 
